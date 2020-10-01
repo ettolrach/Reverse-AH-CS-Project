704e30acd4a3b43fa4dcf7a780d66b537a8b9ba6
Patrick Ausel <TheSheepGuy1@gmail.com>
2020-10-01 21:27:54 +0100

    Fix wrong starting position

--- reverso.py
+++ reverso.py
@@ -23,8 +23,8 @@ def set_up_board(screen):
     newBoard = [None for i in range(BOARD_SIZE**2)]
     # Use i = x + wy where i is the desired index in the 1D list, x and y are coordinates, and w is the width.
     # Also note that the y-axis goes from top to bottom, not bottom to top like how it usually does.
-    newBoard[3 + 8*3] = classes.Disc(False, RIGHT_OFFSET + 3 * SQUARE_SIZE, TOP_OFFSET + 3 * SQUARE_SIZE)
-    newBoard[4 + 8*3] = classes.Disc(True, RIGHT_OFFSET + 4 * SQUARE_SIZE, TOP_OFFSET + 3 * SQUARE_SIZE)
+    newBoard[3 + 8*3] = classes.Disc(True, RIGHT_OFFSET + 3 * SQUARE_SIZE, TOP_OFFSET + 3 * SQUARE_SIZE)
+    newBoard[4 + 8*3] = classes.Disc(False, RIGHT_OFFSET + 4 * SQUARE_SIZE, TOP_OFFSET + 3 * SQUARE_SIZE)
     newBoard[3 + 8*4] = classes.Disc(False, RIGHT_OFFSET + 3 * SQUARE_SIZE, TOP_OFFSET + 4 * SQUARE_SIZE)
     newBoard[4 + 8*4] = classes.Disc(True, RIGHT_OFFSET + 4 * SQUARE_SIZE, TOP_OFFSET + 4 * SQUARE_SIZE)
     newGroup.add(newBoard[3 + 8*3])
