a0a644decd4dd2f5d83bb72d511ba113eb6f4020
Patrick Ausel <TheSheepGuy1@gmail.com>
2020-11-13 12:30:01 +0000

    Add making moves debug code

--- reverso.py
+++ reverso.py
@@ -24,6 +24,19 @@ boardList, boardSpriteGroup, white_counter, black_counter = functions.set_up_boa
 functions.draw_everything(boardSpriteGroup, largeFont, whiteToPlay, white_counter, black_counter)
 pygame.display.update()
 
+
+# To run certain moves for debug purposes, uncomment the following code and replace the example list.
+"""
+moves = ["e6", "f6", "f5", "f4", "f3", "d3", "c3", "c4", "c5", "d6", "e3", "g2", "f7", "c6", "g4", "d2", "b6", "b7", "c7", "e7", "g7", "g6", "g5", "h4", "h3", "h2", "h1", "f2", "g3", "e2", "c2", "b2", "b3", "b4", "b5", "d7", "b8", "c8", "d8", "f8", "g8", "e8", "h6", "h5", "g1", "f1", "e1", "d1", "c1", "b1", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8"]
+
+for move in moves:
+    # Get the x and y coordinates from the pixel value of the mouse click.
+    x = int(ord(move[0]) - 97)
+    y = int(move[1])-1
+    
+    boardList, boardSpriteGroup, whiteToPlay, white_counter, black_counter = functions.make_move(boardList, boardSpriteGroup, largeFont, x, y, whiteToPlay, white_counter, black_counter)
+"""
+
 # Main game loop.
 while stopGame == False:
     for event in pygame.event.get():
